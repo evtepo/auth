9163669085f59f3d5b5184325fc9cad80309417c
evtepo <andrey_laguta00@mail.ru>
2026-03-04 13:58:34 +1000

    fix(user): change registration logic

--- internal/usecase/user/usecase.go
+++ internal/usecase/user/usecase.go
@@ -2,18 +2,42 @@ package user
 
 import (
 	"context"
+	"errors"
 
+	roleDomain "github.com/evtepo/auth/internal/domain/role"
 	userDomain "github.com/evtepo/auth/internal/domain/user"
 )
 
 type Usecase struct {
-	repo userDomain.UserRepository
+	userRepo userDomain.UserRepository
+	roleRepo roleDomain.RoleRepository
 }
 
-func NewUserUsecase(repository userDomain.UserRepository) *Usecase {
-	return &Usecase{repo: repository}
+func NewUserUsecase(userRepo userDomain.UserRepository, roleRepo roleDomain.RoleRepository) *Usecase {
+	return &Usecase{userRepo: userRepo, roleRepo: roleRepo}
 }
 
-func (u *Usecase) Register(ctx context.Context, user *userDomain.User) error {
+func (u *Usecase) Register(
+	ctx context.Context,
+	email userDomain.Email,
+	username userDomain.Username,
+	password userDomain.Password,
+) error {
+	roleName := roleDomain.RoleName("Member")
+	roleId, err := u.roleRepo.FindByName(ctx, roleName)
+	if err != nil {
+		return errors.New("Role not found")
+	}
+	hashedPassword := password
+	entity := &userDomain.User{
+		Email: email,
+		Username: username,
+		Password: hashedPassword,
+		RoleID: roleId.ID,
+	}
+	registerErr := u.userRepo.Register(ctx, entity)
+	if registerErr != nil {
+		return errors.New("Failed to register user")
+	}
 	return nil
 }
