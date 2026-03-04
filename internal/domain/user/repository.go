145abb53da37a9ceeb4afd33a49a95f80a471416
evtepo <andrey_laguta00@mail.ru>
2026-03-04 14:00:36 +1000

    fix(repository): change methods params

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -2,14 +2,13 @@ package user
 
 import (
 	"context"
-
-	. "github.com/evtepo/auth/internal/domain/role"
 )
 
 type UserRepository interface {
 	Register(ctx context.Context, user *User) error
-	ChangeRole(ctx context.Context, user *User, role *Role) error
-	FindById(ctx context.Context, id int) (*User, error)
-	FindByUsername(ctx context.Context, username Usename) (*User, error)
-	GetUserInfo(ctx context.Context, id int) (*User, error)
+	ChangeRole(ctx context.Context, user *User, roleId uint) error
+	FindById(ctx context.Context, id uint) (*User, error)
+	FindByUsername(ctx context.Context, username Username) (*User, error)
+	GetUserInfo(ctx context.Context, req *User) (*User, error)
+	DeleteUser(ctx context.Context, userId uint) error
 }
