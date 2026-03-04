f2b3ca50db206c02a249c9dfaf9654f832d5b4ef
evtepo <andrey_laguta00@mail.ru>
2026-03-04 13:58:54 +1000

    fix(postgres): change user db logic

--- internal/infrastructure/db/postgres/user_pg.go
+++ internal/infrastructure/db/postgres/user_pg.go
@@ -4,16 +4,57 @@ import (
 	"context"
 
 	"github.com/evtepo/auth/internal/domain/user"
+	"gorm.io/gorm"
 )
 
 type UserRepo struct{
-	user.UserRepository
+	db *gorm.DB
 }
 
-func NewRepository() *UserRepo {
-	return &UserRepo{}
+func NewRepository(db *gorm.DB) *UserRepo {
+	return &UserRepo{db: db}
 }
 
 func (r *UserRepo) Register(ctx context.Context, user *user.User) error {
-	return nil
+	user.IsAdmin = false
+	newUser := r.db.Create(&user)
+	return newUser.Error
+}
+
+func (r *UserRepo) ChangeRole(ctx context.Context, user *user.User, roleId uint) error {
+	user.RoleID = roleId
+	result := r.db.Save(&user)
+	return result.Error
+}
+
+func (r * UserRepo) FindById(ctx context.Context, id uint) (*user.User, error) {
+	var foundUser user.User
+	result := r.db.First(&foundUser, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &foundUser, nil
+}
+
+func (r *UserRepo) FindByUsername(ctx context.Context, username user.Username) (*user.User, error) {
+	var foundUser user.User
+	result := r.db.Where("username = ?", username).First(&foundUser)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &foundUser, nil
+}
+
+func (r *UserRepo) GetUserInfo(ctx context.Context, req *user.User) (*user.User, error) {
+	var foundUser user.User
+	result := r.db.Where("id = ?", req.ID).First(&foundUser)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &foundUser, nil
+}
+
+func (r *UserRepo) DeleteUser(ctx context.Context, userId uint) error {
+	result := r.db.Delete(&user.User{}, userId)
+	return result.Error
 }
