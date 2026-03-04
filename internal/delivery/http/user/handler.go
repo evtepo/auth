66a37a42183c8fd72310ea14c225b8905207c276
evtepo <andrey_laguta00@mail.ru>
2026-03-04 13:59:36 +1000

    feat(handler): add user registration handler

--- internal/delivery/http/user/handler.go
+++ internal/delivery/http/user/handler.go
@@ -1,1 +1,40 @@
 package user
+
+import (
+	userDomain "github.com/evtepo/auth/internal/domain/user"
+	"github.com/evtepo/auth/internal/usecase/user"
+	"github.com/gin-gonic/gin"
+	baseUtils "github.com/evtepo/auth/internal/utils"
+)
+
+type Handler struct {
+	uc *user.Usecase
+}
+
+func (h *Handler) RegisterUserRoutes(r *gin.Engine) {
+	group := r.Group("/api/v1/user")
+	group.POST("/register", h.Register)
+}
+
+func NewHandler(uc *user.Usecase) *Handler {
+	return &Handler{uc: uc}
+}
+
+func (h *Handler) Register(c *gin.Context) {
+	var req RegisterRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": baseUtils.ValidateError(err), "success": false})
+		return
+	}
+
+	email := userDomain.Email(req.Email)
+	username := userDomain.Username(req.Username)
+	password := userDomain.Password(req.Password)
+
+	err := h.uc.Register(c.Request.Context(), email, username, password)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error(), "success": false})
+	}
+
+	return
+}
