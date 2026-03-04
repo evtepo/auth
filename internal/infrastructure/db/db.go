32c1f99df2525f3981efb8b43a702b45fdc2428b
evtepo <andrey_laguta00@mail.ru>
2026-03-04 14:01:00 +1000

    fix(db): add simple migration logic

--- internal/infrastructure/db/db.go
+++ internal/infrastructure/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"github.com/evtepo/auth/internal/infrastructure/db/config"
+	migration "github.com/evtepo/auth/internal/infrastructure/db/postgres"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,11 +11,14 @@ import (
 func BuildConnection(config *config.DBConfig) *gorm.DB {
 	gormConfig := &gorm.Config{}
 	db, err := gorm.Open(postgres.Open(config.FullDNS()), gormConfig)
-	if err != nil {
-		panic(err)
+	if err == nil {
+		if config.Migration {
+			migration.Migrate(db)
+		}
+		return db
 	}
 
-	return db
+	panic(err.Error())
 }
 
 func CloseConnection(db *gorm.DB) error {
